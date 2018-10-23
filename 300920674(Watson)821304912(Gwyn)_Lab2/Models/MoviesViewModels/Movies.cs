f9ae9e01861c0276c08489f3b9302025786d9655
Ryan <ryan.bg.watson@gmail.com>
2018-10-23 16:25:20 -0400

    Tried scaffolding

---
+++ 300920674(Watson)821304912(Gwyn)_Lab2/Models/MoviesViewModels/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _300920674_Watson_821304912_Gwyn__Lab2.Controllers
+{
+    public class Movies
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public int Rating { get; set; }
+    }
+}
